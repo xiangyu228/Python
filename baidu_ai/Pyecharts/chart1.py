397402e047ffd9ef6ba26f34b63dcd95c12869cc
xiaoyu228 <2270466620@qq.com>
2018-10-10 14:53:17 +0800

    基于百度AI的文字识别服务

--- baidu_ai/Pyecharts/chart1.py
+++ baidu_ai/Pyecharts/chart1.py
@@ -1,1 +1,12 @@
 __author__ = '井翔宇'
+"""
+  @ 使用Pyecharts绘制图表
+  @ 时间:2018-10-10 13:46
+"""
+import time
+import sys
+from pyecharts import Bar
+bar = Bar("我的第一个图表", "这里是副标题")
+bar.add("服装", ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"], [5, 20, 36, 10, 75, 90],is_more_utils=True)
+bar.show_config()
+bar.render()
