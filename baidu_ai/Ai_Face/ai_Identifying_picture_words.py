7a9bf703cc02ecf4aae8f86ce3bf24b1de9e9f72
xiaoyu228 <2270466620@qq.com>
2018-10-10 12:00:29 +0800

    基于百度AI的文字识别服务

--- baidu_ai/Ai_Face/ai_Identifying_picture_words.py
+++ baidu_ai/Ai_Face/ai_Identifying_picture_words.py
@@ -4,4 +4,47 @@ __author__ = '井翔宇'
   @ 识别图片中的文字信息
   @ 时间:2018-10-10 11:02
 """
+from aip import AipOcr
+import base64
 
+"""
+  @ 初始化新建一个AipOcr
+  @ 并返回结果
+"""
+def AipOcr_(APP_ID, API_KEY, SECRET_KEY):
+    client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
+    return client
+
+""" 读取图片 """
+def get_file_content(filePath):
+    with open(filePath, 'rb') as fp:
+        return fp.read()
+
+"""
+  @ 调用通用文字识别, 图片参数为本地图片
+  @ 测试发现精度不高
+"""
+def general_img(client,img_path):
+    image = get_file_content(img_path)
+    result= client.basicGeneral(image);
+    return result
+
+""" 调用通用文字识别（高精度版） """
+def basicAccurate_img(client,img_path):
+    image = get_file_content(img_path)
+    result= client.basicAccurate(image);
+    return result
+
+# 主体入口
+if __name__ == '__main__':
+
+    APP_ID = '14385953'
+    API_KEY = 'SR9GY7DFqvNklUbkdBhiTADH'
+    SECRET_KEY = 'Hf16YGpQzkGAvl3HEGFNt3crydt6oZs8 '
+
+    image_path = 'img/timg.jpg'
+    client = AipOcr_(APP_ID, API_KEY, SECRET_KEY)
+    #general_result = general_img(client, image_path)            # 通用文字识别
+    basicAccurate_img = basicAccurate_img(client, image_path)    # 通用文字识别(高精度)
+
+    print(basicAccurate_img)
